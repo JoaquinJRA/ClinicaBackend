b1e46216fd8743e3fea346ab7c337df609227b98
JavierAQR <nonamesteam123@gmail.com>
2026-05-18 22:54:05 -0500

    fix: Corregida importación de errorHandler faltante en server.js. Descomentado generator en schema.prisma

--- src/server.js
+++ src/server.js
@@ -1,6 +1,7 @@
 import cookieParser from "cookie-parser";
 import express from "express";
 import authRoutes from "./routes/auth.routes.js";
+import { errorHandler } from "./middlewares/errorHandler.js";
 
 const app = express();
 app.use(express.json());
